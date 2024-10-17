26d512afcad6865957300266e1de418557429f55
LeonidCaminschi <leocer.leonid@gmail.com>
2024-10-17 17:04:40 +0300

    Fixed redis

--- ServiceB/main.py
+++ ServiceB/main.py
@@ -18,7 +18,7 @@ db_config = {
     'port': 3306
 }
 
-redis_client = redis.StrictRedis(host='localhost', port=6666, db=0)
+redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
 def get_db_connection():
     connection = mysql.connector.connect(**db_config)
